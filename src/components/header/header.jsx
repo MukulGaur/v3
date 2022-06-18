2ecdd6e67aa4e854e2f9e2600b963e37fe6c2489
mukul <mukulgaur91@gmail.com>
2022-06-18 23:17:32 +0530

    initial commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './header.css';
+import Logo from '../../assets/logo.png';
+
+const header = () => {
+  return (
+    <>
+      <header>
+        <figure>
+            <img src={Logo} alt="Mukul Gaur" id="logo" />
+        </figure>
+        <nav>
+            <ul>
+                <li>
+                    <span className='menu'>01.</span> ABOUT
+                </li>
+                <li>
+                    <span className='menu'>02.</span> EXPERIENCE
+                </li>
+                <li>
+                    <span className='menu'>03.</span> PROJECTS
+                </li>
+                <li>
+                    <button id='resume'>RESUME</button>
+                </li>
+            </ul>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default header
