a79b9a19b0d84ac97238927a5f392d21329bd1d8
mukul <mukulgaur91@gmail.com>
2022-06-23 22:49:32 +0530

    changed experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,48 +1,91 @@
-import React from 'react';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import './experience.css';
 
-const Experience = () => {
-    
-    const companies = ["Capgemini", "Endurance", "TCS iON"];
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-    return (
-        <>
-            <section id='exp-section'>
-                <div id='exp-heading'>
-                    <h1 className='heading'><span className='heading-number'>02.</span> Where I’ve Worked</h1>
-                    <hr />
-                </div>
-                <div id='exp-content'>
-                    <div id='company-btns'>
-                        {
-                            companies.map( (name, index)=>{
-                                return (
-                                    <button key={index} className='btn'>{name}</button>
-                                )
-                            })
-                        }
-                    </div>
-                    <div className="desc-container">
-                        <div>
-                            <h3>desg @ A</h3>
-                            <h4>Dec 2019 - Present</h4>
-                            <ul>
-                                <li>
-                                    <p>num 1</p>
-                                </li>
-                                <li>
-                                    <p>num 2</p>
-                                </li>
-                                <li>
-                                    <p>num 3</p>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
 
-export default Experience
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+export default function VerticalTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const companies = ["Capgemini", "Endurance", "TCS iON"];
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div id='exp-section'>
+        <div id='article-heading'>
+            <h1 className='heading'><span className='heading-number'>02.</span> Experience</h1>
+            <hr />
+        </div>
+        <Box
+        sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
+        >
+        <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={value}
+            onChange={handleChange}
+            aria-label="Vertical tabs example"
+            sx={{ borderRight: 1, borderColor: 'divider', overflow: 'inherit' }}
+        >
+            <Tab label="Capgemini" {...a11yProps(0)} />
+            <Tab label="Endurance" {...a11yProps(1)} />
+            <Tab label="TCS iON" {...a11yProps(2)} />
+            
+        </Tabs>
+        <TabPanel value={value} index={0}>
+            Working as a Full Stack developer on technologies like React JS, Spring framework, and MySQL.
+            Developed REST API for ticketing system, and restructured the existing APIs to increase the efficiency by 20%.
+            Performed regular peer code reviews.
+            Tech Stack - React JS, Java, Spring, Postman, REST API.
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+            Responsible for resolving high-priority tickets of web-hosting and email hosting products.
+            Server administration & health checkups.
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+            Responsible for building an Android freelancer mobile application tool.
+            Designed wireframes and was also responsible for creating the entire process flow of the application.
+            Organized meet-ups to exchange our ideas.
+        </TabPanel>
+        </Box>
+    </div>
+  );
+}
